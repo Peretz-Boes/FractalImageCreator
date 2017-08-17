c42afb3332f4cf987c06b33acd1f5623d3c52ebf
Peretz Boes <peretzboes@gmail.com>
2017-08-17 15:07:28 -0400

    post changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
+//============================================================================
+// Name        : Fractal.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
+
 #include "FractalCreator.h"
 #include "RGB.h"
+#include "Zoom.h"
 
 using namespace std;
 using namespace caveofprogramming;
 
 int main() {
+
 	FractalCreator fractalCreator(800, 600);
 
 	fractalCreator.addRange(0.0, RGB(0, 0, 255));
